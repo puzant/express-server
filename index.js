9297757fdba926cb76c384bc7e3f111b23330d83
Puzant <PBAKJEJIAN@hotmail.com>
2019-12-01 17:26:03 +0200

    add new routes for testing

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const upload = multer()
 const PORT = 5000
 
 const router = require('./router.js')
 
 app.set('view engine', 'pug')
 
+//  for parsing application/json
+app.use(bodyParser.json())
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }))
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array())
+app.use(express.static('views'))
+
+//  add this line after all the midllewares in express
 app.use('/home' ,router)
 
 //  show this text in when we visit this URL(localhost:5000/)
